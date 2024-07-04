3f2c6c1b45bf0d2a428fd0320550fa22b37a96d4
Darius <103670474+Dari-OS@users.noreply.github.com>
2024-07-04 20:18:55 +0000

    added error hinting in insert mode

--- .config/nvim/lua/dari-os/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/dari-os/plugins/lsp/lspconfig.lua
@@ -8,16 +8,18 @@ return {
 	},
 
 	config = function()
-		-- import lspconfig plugin
 		local lspconfig = require("lspconfig")
-
-		-- import mason_lspconfig plugin
 		local mason_lspconfig = require("mason-lspconfig")
+		local cmp_nvim_lsp = require("cmp_nvim_lsp")
+		local keymap = vim.keymap
 
-		-- In your lspconfig.lua file, replace the existing rust_analyzer setup with this:
+		-- Enable Clippy for Rust
 		lspconfig.rust_analyzer.setup({
 			settings = {
 				["rust-analyzer"] = {
+					checkOnSave = {
+						command = "clippy",
+					},
 					diagnostics = {
 						enable = true,
 						experimental = {
@@ -34,10 +36,10 @@ return {
 			},
 		})
 
-		-- import cmp-nvim-lsp plugin
-		local cmp_nvim_lsp = require("cmp_nvim_lsp")
-
-		local keymap = vim.keymap -- for conciseness
+		-- Enable error hints in insert mode
+		vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+			update_in_insert = true,
+		})
 
 		vim.api.nvim_create_autocmd("LspAttach", {
 			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
@@ -88,30 +90,25 @@ return {
 			end,
 		})
 
-		-- used to enable autocompletion (assign to every lsp server config)
 		local capabilities = cmp_nvim_lsp.default_capabilities()
 
-		-- Change the Diagnostic symbols in the sign column (gutter)
-		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
 		for type, icon in pairs(signs) do
 			local hl = "DiagnosticSign" .. type
 			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 		end
 
 		mason_lspconfig.setup_handlers({
-			-- default handler for installed servers
 			function(server_name)
 				lspconfig[server_name].setup({
 					capabilities = capabilities,
 				})
 			end,
 			["lua_ls"] = function()
-				-- configure lua server (with special settings)
 				lspconfig["lua_ls"].setup({
 					capabilities = capabilities,
 					settings = {
 						Lua = {
-							-- make the language server recognize "vim" global
 							diagnostics = {
 								globals = { "vim" },
 							},
