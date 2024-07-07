179f9e49806679d7ab8ce926e78c4a5bb51fb3bd
Darius <103670474+Dari-OS@users.noreply.github.com>
2024-07-07 10:13:48 +0000

    Fixed an annoying issue when pressing space in the insert mode

--- .config/nvim/lua/dari-os/plugins/toggleterm.lua
+++ .config/nvim/lua/dari-os/plugins/toggleterm.lua
@@ -7,6 +7,29 @@ return {
 			open_mapping = [[<leader>t]],
 			direction = "vertical",
 			shade_terminals = true,
+			-- Specify that the mapping should only work in normal mode
+			on_open = function(term)
+				vim.cmd("startinsert!")
+				vim.api.nvim_buf_set_keymap(
+					term.bufnr,
+					"n",
+					"<leader>t",
+					"<cmd>ToggleTerm<CR>",
+					{ noremap = true, silent = true }
+				)
+				vim.api.nvim_buf_set_keymap(
+					term.bufnr,
+					"t",
+					"<leader>t",
+					"<cmd>ToggleTerm<CR>",
+					{ noremap = true, silent = true }
+				)
+			end,
+			-- Disable the default insert mode mapping
+			insert_mappings = false,
 		})
+
+		-- Set the mapping only for normal mode globally
+		vim.api.nvim_set_keymap("n", "<leader>t", "<cmd>ToggleTerm<CR>", { noremap = true, silent = true })
 	end,
 }
