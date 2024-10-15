345a6ae41fe68f4a4a12cf790d4652117229a680
dari-os <Darius.dev@proton.me>
2024-10-15 20:32:03 +0200

    Added wezterm config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,32 @@
+local wezterm = require("wezterm")
+
+return {
+	-- Font configuration
+	font = wezterm.font("JetBrains Mono"),
+
+	-- Color scheme
+	color_scheme = "Catppuccin Macchiato",
+
+	window_padding = {
+		left = 10,
+		right = 10,
+		top = 0,
+		bottom = 0,
+	},
+
+	-- Key bindings
+	keys = {
+		-- F11 to toggle maximized state
+		-- ISSUE: https://github.com/wez/wezterm/issues/6244
+		-- {key="F11", mods="", action=wezterm.action.ToggleMaximized},
+
+		-- Ctrl+F11 to toggle fullscreen
+		{ key = "F11", mods = "CTRL", action = wezterm.action.ToggleFullScreen },
+	},
+
+	-- Disable tab bar
+	enable_tab_bar = false,
+
+	-- Disable scrollbar
+	enable_scroll_bar = false,
+}
